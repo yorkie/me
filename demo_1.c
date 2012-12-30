040c4ec81b0d95dc55c704b1ad90a653c0155278
Yorkie <l900422@gmail.com>
2012-12-30 21:58:52 +0800

    Update demo_1.c

--- demo_1.c
+++ demo_1.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
+#define MAX 20
 
 void main() {
   
-  int max = 20;
-  int a[max], b[10] = {0}, temp = -1;
-  printf("亲，请输入%d个10以内的正整数\n", max);
+  int MAX = 20;
+  int a[MAX], b[10] = {0}, temp = -1;
+  printf("亲，请输入%d个10以内的正整数\n", MAX);
 
-  for (int i = 0; i < max;) {
-    scanf("%d", &temp);
+  for (int i = 0; i < MAX;) {
+    scanf_s("%d", &temp);
     if (temp > 0 && temp < 10) {
       a[i++] = temp;
       b[temp]++;
-      printf("%d/%d", i, max);
+      printf("%d/%d\n", i, MAX);
     }
     else {
-      printf("请重新输入一个10以内的正整数");
+      printf("请重新输入一个10以内的正整数\n");
     }
   }
   
   for (int i = 0; i < 10; i++) {
-    printf("%d的出现次数：%d", i, b[i]);
+    printf("%d的出现次数：%d，\n", i, b[i]);
   }
-  getch();
+  scanf_s("%d", &temp);
 
 }
