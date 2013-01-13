ea8a52f4f699bf083ae5fbafd9df71d41fbf367b
Yorkie <l900422@gmail.com>
2013-01-14 04:58:44 +0800

    tweaks

--- _tools/PageBuilder/index.js
+++ _tools/PageBuilder/index.js
@@ -3,11 +3,12 @@ var jshtml = require("jshtml");
 
 var output = "";
 var write = function(lineStr) {
+	console.log(lineStr + ':rf');
 	output += lineStr.replace(/\r\n\t/g, '');
 }
 var end = function() {
 	console.log(output);
 }
 
-var templateValue = fs.readFileSync(__dirname + '/../../_res/blogs/list.jshtml', 'utf8');
+var templateValue = fs.readFileSync(__dirname + '/../../_views/blogs/list.jshtml', 'utf8');
 jshtml.renderAsync(write, end, templateValue, {});
