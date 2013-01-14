5001af7c61f9a9ada1024a28f0c6d21d6d7748a9
Yorkie <l900422@gmail.com>
2013-01-15 00:11:08 +0800

    Update javascripts/test_require.js

--- javascripts/test_require.js
+++ javascripts/test_require.js
@@ -1,5 +1,5 @@
-define(["https://raw.github.com/enginespot/browser-buffer/master/src/browser-buffer.js"], function(){
-  return {
-    a: new Buffer(32)
-  };
+define(function(require, exports, module){
+  
+  var a = new Buffer(32);
+  
 });
