9ea20279e1b5c3027579a0bda59627258dc6279e
Yorkie <l900422@gmail.com>
2013-01-14 23:55:16 +0800

    Update javascripts/test_require.js

--- javascripts/test_require.js
+++ javascripts/test_require.js
@@ -1,4 +1,4 @@
-define(["https://raw.github.com/arextar/browser-buffer/master/src/browser-buffer.js"], function(){
+define(["https://raw.github.com/enginespot/browser-buffer/master/src/browser-buffer.js"], function(){
   return {
     a: new Buffer(32)
   };
