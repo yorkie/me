d9188f80bb67ec2cbb7f4a66b8c097cababe9a7e
Yorkie <l900422@gmail.com>
2013-01-14 18:05:56 +0800

    2

---
+++ javascripts/config.js
@@ -0,0 +1,10 @@
+define(function (require, exports) {
+  
+  exports.catalog = [
+    { 'id': 'Closure-Templates', 'name': null },
+    { 'id': 'ECMAScript', 'name': null },
+    { 'id': 'Project', 'name': null},
+    { 'id': 'Repost', 'name': 'ÕªÂ¼/×ªÔØ/Á´½Ó' }
+  ]
+
+});
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,19 @@
 
-seajs.use(['core/base'], function (require) {
+seajs.use(['config'], function (config) {
   
+  // render sidebar UI
+  renderSidebar(config.catalog);
+
 });
+
+function renderSidebar(list) {
+  var elem = document.querySelector('#sidebar>ul');
+  for (var i = 0, len = list.length; i < len; i++) {
+    var li = document.createElement('li');
+    var anchor = document.createElement('a');
+    anchor.href = '#';
+    anchor.innerText = list[i].name ? list[i].name : list[i].id;
+    li.appendChild(anchor);
+    elem.appendChild(li);
+  }
+}
