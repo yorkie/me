123da7bc770cb909f59772817940574e7ca2c89c
Yorkie <l900422@gmail.com>
2013-02-23 01:43:34 +0800

    wandoujia

--- wandoujia/group.js
+++ wandoujia/group.js
@@ -44,6 +44,7 @@ function getDateStringByFormat(date, format) {
 	var reg = /(y{4})|(M{2})|(d{2})|(h{2})|(m{2})|(s{2})/g
 	var result = format.replace(reg, function(src) {
 		console.log(arguments)
+		debugger
 		return methodsMap[src]()
 	})
 	return result;
