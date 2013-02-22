3d616d4e4e5b0f361bd15be9955024bb589cb2b7
Yorkie <l900422@gmail.com>
2013-02-23 01:15:08 +0800

    wandoujia

---
+++ wandoujia/group.js
@@ -0,0 +1,49 @@
+ 
+/* 图片分组 */
+
+function group(photos) {
+
+	var result = {}
+	var max = photos.length
+	var temp, time;
+
+	for (var i=0; i<max; i++) {
+
+		temp = photos[i]
+		time = getDateStringByFormat(temp.time, 'yyyy-MM-dd')
+
+		if (!result[time] || Object.prototype.toString.call(result[time]) !=  '[object Array]') {
+			result[time] = [temp]
+			continue
+		}
+		result[time].push(temp)
+	}
+
+}
+
+/**
+ * 时间按格式转换
+ */
+
+function getDateStringByFormat(date, format) {
+
+	var methodsMap = {
+		'yyyy': date.getFullYear,
+		'MM': function() { return date.getMonth() + 1 },
+		'dd': date.getDate,
+		'hh': date.getHours,
+		'mm': date.getMinutes,
+		'ss': date.getSeconds
+	}
+	var reg = /(y{4})|(M{2})|(d{2})|(h{2})|(m{2})|(s{2})/
+	var result = format.replace(reg, function(src) {
+
+		if (Object.prototype.toString.call(src) != '[object Array]') {
+			return
+		}
+		return methodsMap[src[0]]()
+
+	})
+	return result;
+
+}
